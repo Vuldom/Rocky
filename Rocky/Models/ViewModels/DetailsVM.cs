4470792bdb4f837fbe80ba43cc09a89286d80a38
vuldom <maryia.paliashchuk@gmail.com>
2021-12-29 12:20:07 +0300

    Added view model for Details

---
+++ Rocky/Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+
+        }
+        public Product Product { get; set; }
+        public bool ExcistsInCart { get; set; }
+    }
+}
