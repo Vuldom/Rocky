ce02d41ccdfbf59d88778bed27dcb44801ae4a53
vuldom <maryia.paliashchuk@gmail.com>
2022-01-06 17:39:52 +0300

    EmailSender

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -45,6 +52,40 @@ namespace Rocky.Controllers
 
             HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(t => prodInCart.Contains(t.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+
+            return View(ProductUserVM);
+
         }
     }
 }
---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,62 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("a9e153a29582b1510d60d88c2f7b913d", "a0db5c9f8a3e72a8d3a8f2000b07502d")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+             new JObject {
+              {
+               "From",
+                   new JObject {
+                    {"Email", ""},
+                    {"Name", "Maria"}
+                   }
+                  }, {
+                   "To",
+                   new JArray {
+                    new JObject {
+                     {
+                      "Email",
+                      email
+                     }, {
+                      "Name",
+                      "DotNetMastery"
+                     }
+                    }
+                   }
+                  }, {
+                   "Subject",
+                   subject
+                  },  {
+                   "HTMLPart",
+                  body 
+                  }
+                 }
+             });
+            MailjetResponse response = await client.PostAsync(request);
+        }
+    }
+}
